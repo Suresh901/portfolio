53d52953bf5cacb678f538fba14d2befa41de1d0
Suresh901 <sureshshrestha915@gmail.com>
2024-03-02 21:10:35 +0545

    Footer section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import AboutPage from '@/components/AboutPage'
+import Footer from '@/components/Footer';
 import HeroSection from '@/components/HeroSection'
 import ProjectSection from '@/components/ProjectSection';
 
 const Page = () => {
   return (
-
-
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-6xl ">
       <HeroSection />
       <AboutPage />
       <ProjectSection />
+      <Footer />
 
     </main>
 
