692b05abb536304385cd94967404069ad80cd8cf
Suresh901 <sureshshrestha915@gmail.com>
2024-03-02 21:11:11 +0545

    navlink removed

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,15 +1,15 @@
-"use client"
+// "use client"
 
-import Link from "next/link";
-import { usePathname } from "next/navigation"
+// import Link from "next/link";
+// import { usePathname } from "next/navigation"
 
-const NavLink = ({ link }) => {
-    const pathname = usePathname();
-    return (
-        <Link href={link.url} className={`rounded p-1 ${pathname === link.url && "bg-black text-white"} `}>
-            {link.title}
-        </Link>
-    )
-}
+// const NavLink = ({ link }) => {
+//     const pathname = usePathname();
+//     return (
+//         <Link href={link.url} className={`rounded p-1 ${pathname === link.url && "bg-black text-white"} `}>
+//             {link.title}
+//         </Link>
+//     )
+// }
 
-export default NavLink
+// export default NavLink
