2568d141b5a7f02ec42e4d2c8ec999dca82ed054
Suresh901 <sureshshrestha915@gmail.com>
2024-03-02 22:46:01 +0545

    image changed

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
         <section id='home'>
             <div className="flex flex-col items-center justify-center text-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
                 <div className="md:w-1/2">
-                    <Image src='/image.jpg' alt='' width={400} height={400} className="rounded-full shadow-2xl" />
+                    <Image src='/image.JPG' alt='' width={400} height={400} className="rounded-full shadow-2xl" />
                 </div>
                 <div className="md:mt-2 md:w-3/5">
                     <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl"> Hi, I&#39;m Suresh!</h1>
